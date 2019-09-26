1a57f07085d42a1714b0a84af8645058649afd35
Sandy Akbar <55833676+sandytwelve12@users.noreply.github.com>
2019-09-26 05:44:53 -0700

    Add files via upload

---
+++ PBO-ULANG-10114504-Latihan10-Logika/src/pbo/ulang/pkg10114504/latihan10/logika/PBOULANG10114504Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10114504.latihan10.logika;
+
+/**
+ *
+ * @author
+ * NAMA     : Sandy Akbar
+ * KELAS    : PBO-Ulang
+ * NIM      : 10114504
+ * DESKRIPSI: Program ini berisi program logika
+ */
+public class PBOULANG10114504Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        Boolean dewasa;
+        dewasa = (umur >=17);
+        System.out.println("Status Dewasa : " + dewasa );
+    }
+    
+}
